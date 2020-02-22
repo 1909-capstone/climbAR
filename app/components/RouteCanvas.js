0f823e5a37c04042eb1629ec2e8ae7fc00de54c5
Nicolas Ries <nries1@gmail.com>
2020-02-22 15:23:23 -0500

    added height and widht adjusters for canvas

--- app/components/RouteCanvas.js
+++ app/components/RouteCanvas.js
@@ -3,6 +3,7 @@ import style from '../css/routeCanvas.css';
 import CanvasSlot from './CanvasSlot';
 import { connect } from 'react-redux';
 import { setNewHold } from '../redux/thunks/holdThunks';
+import { Form } from 'react-bootstrap';
 
 class RouteCanvas extends React.Component {
   constructor() {
@@ -11,6 +12,10 @@ class RouteCanvas extends React.Component {
       width: 15,
       height: 20
     };
+    this.handleInput = this.handleInput.bind(this);
+  }
+  handleInput(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
   render() {
     const {
@@ -19,7 +24,35 @@ class RouteCanvas extends React.Component {
     } = this;
     return (
       <div>
-        <div>Canvas</div>
+        <div>
+          Canvas
+          <div>
+            <Form.Group>
+              <Form.Label htmlFor="height">Height</Form.Label>
+              <Form.Control
+                id="height"
+                name="height"
+                type="number"
+                min="0"
+                max="7"
+                value={this.state.height}
+                onChange={this.handleInput}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="width">Width</Form.Label>
+              <Form.Control
+                id="width"
+                name="width"
+                type="number"
+                min="0"
+                max="3"
+                value={this.state.width}
+                onChange={this.handleInput}
+              />
+            </Form.Group>
+          </div>
+        </div>
         <div
           className="route_canvas"
           style={{ width: `${width}em`, height: `${height}em` }}
