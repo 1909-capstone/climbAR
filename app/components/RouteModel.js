7e90c88f3c75fdaa391a6a49401e073e6bb2d14b
Nicolas Ries <nries1@gmail.com>
2020-02-24 15:55:26 -0500

    generate a wall in front of the user based on teh dimensions of the climbing route entity

--- app/components/RouteModel.js
+++ app/components/RouteModel.js
@@ -1,5 +1,4 @@
 import 'aframe';
-import 'aframe-particle-system-component';
 import {Entity, Scene} from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -15,17 +14,18 @@ class RouteModel extends React.Component {
     const { climbingRoute } = this.props;
     return (<Scene>
     <a-assets>
-          <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" crossOrigin="anonymous"/>
-          <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg" crossOrigin="anonymous"/>
+          <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg" />
+          <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg" />
     </a-assets>
-    <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
+        <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
         <Entity primitive="a-light" type="ambient" color="#445451"/>
         <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
         <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-        <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
         <Entity text={{value: 'ClimbAr!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
-        <Entity primitive="a-camera">
-          <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
+        {/* TODO: FIND A WAY TO LOAD 3D MODELS IN REACT <Entity gltf-model={{src: '../public/assets/climbing_wall/scene.gltf'} }/> */}
+        <Entity primitive="a-plane" src="#groundTexture" rotation="0 0 0" height={climbingRoute.areaHeight} width={climbingRoute.areaWidth} position="0 0 -1"/>
+        <Entity primitive="a-camera" position="0 1.75 0">
+          {/* <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/> */}
         </Entity>
         </Scene>)
   }
