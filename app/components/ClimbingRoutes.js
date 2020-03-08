75a7844f73526743be4a80537f8a31d7914ca888
Nicolas Ries <nries1@gmail.com>
2020-03-08 12:31:26 -0400

    reditected users to form when they click edit

--- app/components/ClimbingRoutes.js
+++ app/components/ClimbingRoutes.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { fetchClimbingRoutes } from '../redux/thunks/climbingRoutesThunks';
 import RouteTile from './RouteTile';
 import RouteFilters from './RouteFilters';
+import { setRouteModel } from '../redux/actions';
 
 class ClimbingRoutes extends React.Component {
   constructor() {
@@ -39,7 +40,7 @@ class ClimbingRoutes extends React.Component {
   }
   render() {
     const {
-      props: { climbingRoutes, user },
+      props: { climbingRoutes, user, setRouteModel },
       filter
     } = this;
     return (
@@ -53,6 +54,8 @@ class ClimbingRoutes extends React.Component {
                 key={climbingRoute.id}
                 route={climbingRoute}
                 user={user}
+                route={climbingRoute}
+                setRouteModel={setRouteModel}
               />
             ) : (
               ''
@@ -72,7 +75,8 @@ const mapState = ({ climbingRoutes, user, routeFilters }) => ({
 
 const mapDispatch = dispatch => {
   return {
-    fetchClimbingRoutes: () => dispatch(fetchClimbingRoutes())
+    fetchClimbingRoutes: () => dispatch(fetchClimbingRoutes()),
+    setRouteModel: model => dispatch(setRouteModel(model))
   };
 };
 
--- app/components/RouteTile.js
+++ app/components/RouteTile.js
@@ -6,59 +6,88 @@ import CompleteButton from './CompleteButton';
 import RatingButon from './RatingButton';
 import RatingDisplay from './RatingDisplay';
 import { average } from '../utils';
+import { withRouter } from 'react-router-dom';
 
-const RouteTile = ({ route, user }) => {
-  const avgRating = average(route.ratings, 'rating');
-  const daysToExpire = () => {
-    const expDate = new Date(route.endDate).getTime();
-    const today = new Date().getTime();
-    const hoursToExpire = Math.floor((expDate - today) / (1000 * 60 * 60));
-    if (hoursToExpire <= 24) return 'Today';
-    return `In ${Math.floor(hoursToExpire / 24)} days`;
-  };
-  const darkColor = route =>
-    ['black', 'green', 'purple'].indexOf(route.holdColor) !== -1;
-  return (
-    <div className="routeTile">
-      <div
-        className="color-bar"
-        style={{
-          flexGrow: 1,
-          backgroundColor: route.holdColor,
-          color: darkColor(route) ? 'white' : 'black',
-          fontWeight: '700',
-          width: '100%',
-          height: '2em',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          marginBottom: '0.75em'
-        }}
-      >
-        {route.grade}
-      </div>
-      <div className="route-metrics">
-        <LikeButton route={route} />
-        <CompleteButton route={route} />
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <RatingDisplay avgRating={avgRating} />
-          <small>difficulty</small>
+class RouteTile extends React.Component {
+  constructor() {
+    super();
+    this.editRoute = this.editRoute.bind(this);
+  }
+  editRoute() {
+    this.props.setRouteModel(this.props.route);
+    this.props.history.push('/admin/create');
+  }
+  render() {
+    const { route, user, setRouteModel } = this.props;
+    const component = this;
+    const avgRating = average(route.ratings, 'rating');
+    const daysToExpire = () => {
+      const expDate = new Date(route.endDate).getTime();
+      const today = new Date().getTime();
+      const hoursToExpire = Math.floor((expDate - today) / (1000 * 60 * 60));
+      if (hoursToExpire <= 24) return 'Today';
+      return `In ${Math.floor(hoursToExpire / 24)} days`;
+    };
+    const darkColor = route =>
+      ['black', 'green', 'purple'].indexOf(route.holdColor) !== -1;
+    return (
+      <div className="routeTile">
+        <div
+          className="color-bar"
+          style={{
+            flexGrow: 1,
+            backgroundColor: route.holdColor,
+            color: darkColor(route) ? 'white' : 'black',
+            fontWeight: '700',
+            width: '100%',
+            height: '2em',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginBottom: '0.75em'
+          }}
+        >
+          {route.grade}
         </div>
-        {user.userType && <RatingButon route={route} />}
+        <div className="route-metrics">
+          <LikeButton route={route} />
+          <CompleteButton route={route} />
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <RatingDisplay avgRating={avgRating} />
+            <small>difficulty</small>
+          </div>
+          {user.userType && <RatingButon route={route} />}
+        </div>
+        <div>
+          <i className="large material-icons">schedule</i> Expiring{' '}
+          {daysToExpire()}
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            width: '95%',
+            height: '2.5em',
+            padding: '0 2.5% 0 2.5%',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}
+        >
+          <Link
+            className="btn btn-info btn-sm"
+            key={route.id}
+            to={`/climbingroutes/${route.id}`}
+          >
+            More
+          </Link>
+          <div onClick={this.editRoute}>
+            {user.userType === 'Admin' && (
+              <i className="material-icons">create</i>
+            )}
+          </div>
+        </div>
       </div>
-      <div>
-        <i className="large material-icons">schedule</i> Expiring{' '}
-        {daysToExpire()}
-      </div>
-      <Link
-        className="btn btn-info btn-sm"
-        key={route.id}
-        to={`/climbingroutes/${route.id}`}
-      >
-        More
-      </Link>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default RouteTile;
+export default withRouter(RouteTile);
