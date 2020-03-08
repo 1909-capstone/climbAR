db30cd792025e834ddeb0d51b4d4d9fe36d423ea
Nicolas Ries <nries1@gmail.com>
2020-03-08 17:39:12 -0400

    new-confirmation-page merge

--- app/components/CreateRoute.js
+++ app/components/CreateRoute.js
@@ -7,40 +7,20 @@ import style from '../css/createRoute.css';
 import { Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { createRouteModel } from '../redux/thunks/routeModelThunks';
-import CreateRouteSuccess from './CreateRouteSuccess'; 
+import CreateRouteSuccess from './CreateRouteSuccess';
 import { editRouteModel } from '../redux/thunks/routeModelThunks';
-
 
 class CreateRoute extends React.Component {
   previous = e => {
     e.preventDefault();
     this.props.prevStep();
   };
-  handleClick = inputData => {
-    const { createRouteModel } = this.props;
-    createRouteModel(inputData);
+  handleClick = routeModel => {
+    const { createRouteModel, editRouteModel } = this.props;
+    routeModel.id ? editRouteModel(routeModel) : createRouteModel(routeModel);
     this.props.nextStepDouble();
   };
   render() {
-
-    const { routeModel , climbingRoute } = this.props;
-    return (
-      <Fragment>
-          <div>
-            <div className="create_route">
-              <DndProvider backend={Backend}>
-                <Holds />
-                <RouteCanvas />
-              </DndProvider>
-            </div>
-            <div className="button_container">
-              <Button onClick={this.previous}> Previous </Button>
-              <Button onClick={() => this.handleClick(routeModel)}>
-                Save New Route
-              </Button>
-            </div>
-          </div>
-      </Fragment>
     const { createRouteModel, routeModel, values, editRouteModel } = this.props;
     return (
       <div>
@@ -54,9 +34,7 @@ class CreateRoute extends React.Component {
           <Button onClick={this.previous}> Previous </Button>
           <Button
             onClick={() => {
-              routeModel.id
-                ? editRouteModel(routeModel)
-                : createRouteModel(routeModel);
+              this.handleClick(routeModel);
             }}
           >
             {routeModel.id ? `Save Changes` : `Save New Route`}
@@ -67,7 +45,10 @@ class CreateRoute extends React.Component {
   }
 }
 
-const mapState = ({ routeModel, climbingRoute }) => ({ routeModel, climbingRoute });
+const mapState = ({ routeModel, climbingRoute }) => ({
+  routeModel,
+  climbingRoute
+});
 const mapDispatch = dispatch => {
   return {
     createRouteModel: model => dispatch(createRouteModel(model)),
