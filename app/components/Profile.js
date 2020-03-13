48c88233d63955950c9486c82487de4df3efda73
Cissy Bai <cissy5120@gmail.com>
2020-03-12 21:40:19 -0400

    centered climbing routes title

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -22,20 +22,40 @@ class Profile extends React.Component {
     } = this;
     if (!user.completedRoutes) return;
     return user.completedRoutes.length === 0 ? (
-      <div>
-        You haven't completed any routes yet.{' '}
-        <Link to="/climbingroutes">View Some Routes</Link> and start climbing!
+      <div
+        style={{
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '10px'
+        }}
+      >
+        <h6>
+          You haven't completed any routes yet.{' '}
+          <Link to="/climbingroutes">View Some Routes</Link> and start climbing!
+        </h6>
       </div>
     ) : (
-      <div>
-        Highest Route Completed:
-        {user.completedRouteInfo
-          ? mapCompletedRoutes().reduce((best, route) => {
-              return gradeNumber(route.thisRoute.grade) >= gradeNumber(best)
-                ? route.thisRoute.grade
-                : best;
-            }, 'VB')
-          : 'None completed'}
+      <div
+        style={{
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '10px'
+        }}
+      >
+        <h6>
+          Highest Route Completed:
+          {user.completedRouteInfo
+            ? mapCompletedRoutes().reduce((best, route) => {
+                return gradeNumber(route.thisRoute.grade) >= gradeNumber(best)
+                  ? route.thisRoute.grade
+                  : best;
+              }, 'VB')
+            : 'None completed'}
+        </h6>
       </div>
     );
   }
@@ -44,7 +64,9 @@ class Profile extends React.Component {
     if (!user.completedRouteInfo || !this) return;
     return this.mapCompletedRoutes().length === 0 ? null : (
       <div>
-        <div>Your Completed Routes</div>
+        <h5 style={{ display: 'flex', justifyContent: 'center' }}>
+          Your Completed Routes
+        </h5>
         <div
           style={{
             display: 'flex',
