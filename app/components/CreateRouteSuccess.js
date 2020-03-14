6b92d35abd0a7e5114864c23643de4d8a0d487cf
Cissy Bai <cissy5120@gmail.com>
2020-03-14 12:04:42 -0400

    changed href link to react Link on Create Route Success page and styling

--- app/components/CreateRouteSuccess.js
+++ app/components/CreateRouteSuccess.js
@@ -4,22 +4,42 @@ import { connect } from 'react-redux';
 import { createRouteModel } from '../redux/thunks/routeModelThunks';
 import CreateRoute from './CreateRouteForm';
 import { Nav, Jumbotron, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class CreateRouteSuceess extends Component {
   render() {
-    const {climbingRoute } = this.props;
+    const { climbingRoute } = this.props;
     return (
-      <Jumbotron>
+      <Jumbotron style={{ backgroundColor: '#f0eae3' }}>
         <Container>
           <h1> Thank You for your Submission! </h1>
           <div>
             {' '}
-            Please see <a href="/climbingroutes"> all </a>the Climbing Routes!{' '}
+            Please see{' '}
+            <Link to="/climbingroutes" style={{ color: '#E45720' }}>
+              {' '}
+              all{' '}
+            </Link>
+            the Climbing Routes!{' '}
           </div>
-          <div> Please see the Climbing Route <a href={`/climbingroutes/${climbingRoute.id}`}> you created! </a> </div>
           <div>
             {' '}
-            Create another<a href="/admin/create"> Climbing Route! </a>{' '}
+            Please see the Climbing Route{' '}
+            <Link
+              to={`/climbingroutes/${climbingRoute.id}`}
+              style={{ color: '#E45720' }}
+            >
+              {' '}
+              you created!{' '}
+            </Link>{' '}
+          </div>
+          <div>
+            {' '}
+            Create another
+            <Link to="/admin/create" style={{ color: '#E45720' }}>
+              {' '}
+              Climbing Route!{' '}
+            </Link>{' '}
           </div>
         </Container>
       </Jumbotron>
