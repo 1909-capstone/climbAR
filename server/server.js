6ad4df4a0f0c5462278765b49f1a8bb4ea7a5dba
Nicolas Ries <nries1@gmail.com>
2020-03-17 20:34:51 -0400

    hotfix

--- server/server.js
+++ server/server.js
@@ -106,6 +106,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+app.get('/bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/bundle.js'));
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/style.css'));
+});
+
 //Error-handling endware
 app.use('/', (err, req, res, next) => {
   res
