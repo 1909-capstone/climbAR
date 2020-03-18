15976de7f6c518819c58858bc7113fc1520d6430
Nicolas Ries <nries1@gmail.com>
2020-03-17 20:48:24 -0400

    hotfix

--- server/server.js
+++ server/server.js
@@ -102,16 +102,11 @@ app.use((req, res, next) => {
 
 app.use('/api', require('./api'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-app.get('/bundle.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/bundle.js'));
-});
-
-app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/style.css'));
+app.get('*', (req, res) => {
+  console.log(
+    chalk.yellow('NON API GET REQUEST AT ', req.path, ' || ', req.url)
+  );
+  return res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 //Error-handling endware
